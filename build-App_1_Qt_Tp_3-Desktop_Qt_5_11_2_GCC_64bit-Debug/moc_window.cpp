d34ca3e9b5d8e11dc1ac07f181d47521b01ada19
HardUIKnow <harduiknow@gmail.com>
2020-11-17 09:24:05 +0100

    clean files

--- build-App_1_Qt_Tp_3-Desktop_Qt_5_11_2_GCC_64bit-Debug/moc_window.cpp
+++ build-App_1_Qt_Tp_3-Desktop_Qt_5_11_2_GCC_64bit-Debug/moc_window.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'Drapeau.h'
+** Meta object code from reading C++ file 'window.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "Qt_Tp_01/Drapeau.h"
+#include "../App_1_Qt_Tp_3/window.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'Drapeau.h' doesn't include <QObject>."
+#error "The header file 'window.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.11.2. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -20,25 +20,25 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_Drapeau_t {
+struct qt_meta_stringdata_Window_t {
     QByteArrayData data[1];
-    char stringdata0[8];
+    char stringdata0[7];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_Drapeau_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_Window_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_Drapeau_t qt_meta_stringdata_Drapeau = {
+static const qt_meta_stringdata_Window_t qt_meta_stringdata_Window = {
     {
-QT_MOC_LITERAL(0, 0, 7) // "Drapeau"
+QT_MOC_LITERAL(0, 0, 6) // "Window"
 
     },
-    "Drapeau"
+    "Window"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_Drapeau[] = {
+static const uint qt_meta_data_Window[] = {
 
  // content:
        7,       // revision
@@ -54,7 +54,7 @@ static const uint qt_meta_data_Drapeau[] = {
        0        // eod
 };
 
-void Drapeau::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void Window::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     Q_UNUSED(_o);
     Q_UNUSED(_id);
@@ -62,26 +62,26 @@ void Drapeau::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject Drapeau::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_Drapeau.data,
-      qt_meta_data_Drapeau,  qt_static_metacall, nullptr, nullptr}
+QT_INIT_METAOBJECT const QMetaObject Window::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Window.data,
+      qt_meta_data_Window,  qt_static_metacall, nullptr, nullptr}
 };
 
 
-const QMetaObject *Drapeau::metaObject() const
+const QMetaObject *Window::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *Drapeau::qt_metacast(const char *_clname)
+void *Window::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_Drapeau.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_Window.stringdata0))
         return static_cast<void*>(this);
     return QWidget::qt_metacast(_clname);
 }
 
-int Drapeau::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int Window::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
     return _id;
--- build_Qt_Tp_1_debug/moc_Drapeau.cpp
+++ build_Qt_Tp_1_debug/moc_Drapeau.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'Color.h'
+** Meta object code from reading C++ file 'Drapeau.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "Qt_Tp_01/Color.h"
+#include "../Qt_Tp_01/Drapeau.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'Color.h' doesn't include <QObject>."
+#error "The header file 'Drapeau.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.11.2. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -20,25 +20,25 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_Color_t {
+struct qt_meta_stringdata_Drapeau_t {
     QByteArrayData data[1];
-    char stringdata0[6];
+    char stringdata0[8];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_Color_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_Drapeau_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_Color_t qt_meta_stringdata_Color = {
+static const qt_meta_stringdata_Drapeau_t qt_meta_stringdata_Drapeau = {
     {
-QT_MOC_LITERAL(0, 0, 5) // "Color"
+QT_MOC_LITERAL(0, 0, 7) // "Drapeau"
 
     },
-    "Color"
+    "Drapeau"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_Color[] = {
+static const uint qt_meta_data_Drapeau[] = {
 
  // content:
        7,       // revision
@@ -54,7 +54,7 @@ static const uint qt_meta_data_Color[] = {
        0        // eod
 };
 
-void Color::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void Drapeau::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     Q_UNUSED(_o);
     Q_UNUSED(_id);
@@ -62,26 +62,26 @@ void Color::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject Color::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_Color.data,
-      qt_meta_data_Color,  qt_static_metacall, nullptr, nullptr}
+QT_INIT_METAOBJECT const QMetaObject Drapeau::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Drapeau.data,
+      qt_meta_data_Drapeau,  qt_static_metacall, nullptr, nullptr}
 };
 
 
-const QMetaObject *Color::metaObject() const
+const QMetaObject *Drapeau::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *Color::qt_metacast(const char *_clname)
+void *Drapeau::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_Color.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_Drapeau.stringdata0))
         return static_cast<void*>(this);
     return QWidget::qt_metacast(_clname);
 }
 
-int Color::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int Drapeau::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
     return _id;
